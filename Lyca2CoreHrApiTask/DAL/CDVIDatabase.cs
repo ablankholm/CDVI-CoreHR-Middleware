bc7e5c0912f7c24ba71d8f5059cea3b22dbd5a22
ablankholm <bo.bojsen@gmail.com>
2017-07-04 11:21:03 +0100

    Added logging, db repository model

---
+++ Lyca2CoreHrApiTask/DAL/CDVIDatabase.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lyca2CoreHrApiTask.Models;
+using System.Data;
+using NPoco;
+using NLog;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Linq.Expressions;
+
+namespace Lyca2CoreHrApiTask.DAL
+{
+    public class CDVIDatabase
+    {
+        private static Logger log = LogManager.GetCurrentClassLogger();
+        private static string connectionString = ConfigurationManager.AppSettings["CDVI:ConnectionString"];
+
+
+
+        public static List<Event> GetAccessEvents(DateTime from, DateTime to)
+        {
+            List<Event> events = new List<Event>();
+            string From = from.ToString("yyyy-MM-dd HH:mm:ss.fff");
+            string To   = to.ToString("yyyy-MM-dd HH:mm:ss.fff");
+            Console.WriteLine($"from : {From} / {from}, to: {To} / {to}");
+            log.Info($"from : {From} / {from}, to: {To} / {to}");
+            string query = $"SELECT  [Event ID],[Event Type],[Field Time],[Logged Time],[Operator ID],[Card Holder ID],[Record Name ID],[Site Name ID],[UserNameID]"
+                + $"  FROM [Centaur3Events].[dbo].[Events]"
+                + $"  WHERE ([Field Time] BETWEEN CONVERT(datetime, '{From}') AND CONVERT(datetime, '{To}'))" 
+                + $"      AND [Event Type] IN (1280,1288,1313)";
+            using (var conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                using (IDatabase db = new Database(conn))
+                {
+                    var result = db.Fetch<Event>(query);
+                    events = result;
+                }
+                conn.Close();
+            }
+            return events;
+        }
+
+
+
+        public static List<Event> GetEvents(DateTime from, DateTime to, List<int> eventTypes)
+        {
+            List<Event> events = new List<Event>();
+            string From = from.ToString("yyyy-MM-dd HH:mm:ss.fff");
+            string To = to.ToString("yyyy-MM-dd HH:mm:ss.fff");
+            string EventTypes = String.Join(",", eventTypes);
+            Console.WriteLine($"from : {From} / {from}, to: {To} / {to}");
+            log.Info($"from : {From} / {from}, to: {To} / {to}");
+            Console.WriteLine($"types: {EventTypes}");
+            log.Info($"types: {EventTypes}");
+            string query = $"SELECT  [Event ID],[Event Type],[Field Time],[Logged Time],[Operator ID],[Card Holder ID],[Record Name ID],[Site Name ID],[UserNameID]"
+                + $"  FROM [Centaur3Events].[dbo].[Events]"
+                + $"  WHERE ([Field Time] BETWEEN CONVERT(datetime, '{From}') AND CONVERT(datetime, '{To}'))"
+                + $"      AND [Event Type] IN ({EventTypes})";
+            using (var conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                using (IDatabase db = new Database(conn))
+                {
+                    var result = db.Fetch<Event>(query);
+                    events = result;
+                }
+                conn.Close();
+            }
+            return events;
+        }
+
+
+
+        public static List<Event> GetEventsAfter(DateTime pointInTime, List<int> eventTypes)
+        {
+            List<Event> events = new List<Event>();
+            string PointInTime = pointInTime.ToString("yyyy-MM-dd HH:mm:ss.fff");
+            string EventTypes = String.Join(",", eventTypes);
+            string query = $"SELECT  [Event ID],[Event Type],[Field Time],[Logged Time],[Operator ID],[Card Holder ID],[Record Name ID],[Site Name ID],[UserNameID]"
+                + $"  FROM [Centaur3Events].[dbo].[Events]"
+                + $"  WHERE ([Field Time] > CONVERT(datetime, '{PointInTime}'))"
+                + $"      AND [Event Type] IN ({EventTypes})";
+            using (var conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                using (IDatabase db = new Database(conn))
+                {
+                    var result = db.Fetch<Event>(query);
+                    events = result;
+                }
+                conn.Close();
+            }
+            return events;
+        }
+
+
+
+        public static List<Event> GetEventsAfter(uint eventId, List<int> eventTypes)
+        {
+            List<Event> events = new List<Event>();
+            string EventTypes = String.Join(",", eventTypes);
+            string query = $"SELECT  [Event ID],[Event Type],[Field Time],[Logged Time],[Operator ID],[Card Holder ID],[Record Name ID],[Site Name ID],[UserNameID]"
+                + $"  FROM [Centaur3Events].[dbo].[Events]"
+                + $"  WHERE ([Event ID] > {eventId})"
+                + $"      AND [Event Type] IN ({EventTypes})";
+            using (var conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                using (IDatabase db = new Database(conn))
+                {
+                    var result = db.Fetch<Event>(query);
+                    events = result;
+                }
+                conn.Close();
+            }
+            return events;
+        }
+
+
+
+        public static List<Event> GetEventsAfterInclusive(DateTime pointInTime, List<int> eventTypes)
+        {
+            List<Event> events = new List<Event>();
+            string PointInTime = pointInTime.ToString("yyyy-MM-dd HH:mm:ss.fff");
+            string EventTypes = String.Join(",", eventTypes);
+            string query = $"SELECT  [Event ID],[Event Type],[Field Time],[Logged Time],[Operator ID],[Card Holder ID],[Record Name ID],[Site Name ID],[UserNameID]"
+                + $"  FROM [Centaur3Events].[dbo].[Events]"
+                + $"  WHERE ([Field Time] >= CONVERT(datetime, '{PointInTime}'))"
+                + $"      AND [Event Type] IN ({EventTypes})";
+            using (var conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                using (IDatabase db = new Database(conn))
+                {
+                    var result = db.Fetch<Event>(query);
+                    events = result;
+                }
+                conn.Close();
+            }
+            return events;
+        }
+
+
+
+        public static List<Event> GetEventsAfterInclusive(uint eventId, List<int> eventTypes)
+        {
+            List<Event> events = new List<Event>();
+            string EventTypes = String.Join(",", eventTypes);
+            string query = $"SELECT  [Event ID],[Event Type],[Field Time],[Logged Time],[Operator ID],[Card Holder ID],[Record Name ID],[Site Name ID],[UserNameID]"
+                + $"  FROM [Centaur3Events].[dbo].[Events]"
+                + $"  WHERE ([Event ID] >= {eventId})"
+                + $"      AND [Event Type] IN ({EventTypes})";
+            using (var conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                using (IDatabase db = new Database(conn))
+                {
+                    var result = db.Fetch<Event>(query);
+                    events = result;
+                }
+                conn.Close();
+            }
+            return events;
+        }
+
+
+
+        public static List<Event> GetUserEvents(DateTime from, DateTime to, List<int> eventTypes, List<int> userIDs)
+        {
+            List<Event> events = new List<Event>();
+            string From = from.ToString("yyyy-MM-dd HH:mm:ss.fff");
+            string To = to.ToString("yyyy-MM-dd HH:mm:ss.fff");
+            string EventTypes = String.Join(",", eventTypes);
+            string UserIDs = String.Join(",", userIDs);
+            Console.WriteLine($"User id: {UserIDs}");
+            log.Info($"User id: {UserIDs}");
+            string query = $"SELECT  [Event ID],[Event Type],[Field Time],[Logged Time],[Operator ID],[Card Holder ID],[Record Name ID],[Site Name ID],[UserNameID]"
+                + $"  FROM [Centaur3Events].[dbo].[Events]"
+                + $"  WHERE ([Field Time] BETWEEN CONVERT(datetime, '{From}') AND CONVERT(datetime, '{To}'))"
+                + $"      AND [Event Type] IN ({EventTypes})" 
+                + $"      AND [UserNameID] IN ({UserIDs})";
+            using (var conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                using (IDatabase db = new Database(conn))
+                {
+                    var result = db.Fetch<Event>(query);
+                    events = result;
+                }
+                conn.Close();
+            }
+            return events;
+        }
+
+
+
+        public static List<Event> GetYesterdaysEvents()
+        {
+            List<Event> events = new List<Event>();
+            string query = "SELECT  [Event ID],[Event Type],[Field Time],[Logged Time],[Operator ID],[Card Holder ID],[Record Name ID],[Site Name ID],[UserNameID]"
+                            + "  FROM [Centaur3Events].[dbo].[Events]"
+                            + "  WHERE CAST([Field Time] as date) = DATEADD(day, -1, convert(date, GETDATE()))";
+
+            using (var conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                using (IDatabase db = new Database(conn))
+                {
+                    var result = db.Fetch<Event>(query);
+                    events = result;
+                }
+                conn.Close();
+            }
+            return events;
+        }
+    }
+}
--- Lyca2CoreHrApiTask/Program.cs
+++ Lyca2CoreHrApiTask/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NPoco;
+using NLog;
 using Newtonsoft.Json;
 using Lyca2CoreHrApiTask.Models;
 using System.IO;
@@ -11,22 +12,38 @@ using System.Data.Common;
 using System.Data.SqlClient;
 using System.Windows.Forms;
 using System.Configuration;
+using Lyca2CoreHrApiTask.DAL;
+using Microsoft.Extensions.CommandLineUtils;
 
 namespace Lyca2CoreHrApiTask
 {
     class Program
     {
-        static void Main(string[] args)
+        private static Logger log = LogManager.GetCurrentClassLogger();
+        private static string appPath = Application.StartupPath;
+        private static CDVIDatabase db = new CDVIDatabase();
+
+        static int Main(string[] args)
         {
-            string      connectionString    = ConfigurationManager.AppSettings["CDVI:ConnectionString"];
-            string      appPath             = Application.StartupPath;
-            List<Event> Events              = new List<Event>();
+            var app = new CommandLineApplication
+            {
+                Name = "Lyca2CoreHrApiTask",
+                Description = "Middleware that pulls together access control data from legacy systems and posts it to the CoreHR clocking API",
+                FullName = "Lyca to CoreHR Clocking API Automated Middleware"
+            };
+
+            app.OnExecute(() => 
+            {
+                app.ShowHelp();
+                Test();
+                return 2;
+            });
 
             //Retrieve configuration
             //@TODO
 
             //Retrieve data from CDVI DB
-            Events = GetYesterdaysEvents(connectionString);
+            //@TODO
 
             //Prepare data
             //@TODO
@@ -34,53 +51,48 @@ namespace Lyca2CoreHrApiTask
             //Post data to CoreHr API
             //@TODO
 
-            //Conduct testing
-            Test(appPath, Events);
+            //Iterative testing during dev
 
+            app.Command("RunDevTest", c => {
+
+                c.Description = "Runs the currently configured test method for iterative development";
+
+                c.HelpOption("-?|-h|--help");
+
+                c.OnExecute(() => {
+
+                    Test();
+
+                    return 0;
+                });
+
+            });
+
+            return app.Execute(args);
         }
 
 
 
-        //@Testing
-        static void Test(string path, List<Event> events)
+        //@TempTesting
+        static void Test()
         {
-            string eventOutput = $"UserID: {events.FirstOrDefault().UserNameID.ToString()}" + $"Count: {events.Count.ToString()}";
+            List<Event> events = new List<Event>();
+            DateTime today = DateTime.Today;
+            DateTime from = new DateTime(today.Year, today.Month, today.Day, 0, 0, 0, 0);
+            DateTime to = new DateTime(today.Year, today.Month, today.Day, 23, 59, 59, 999);
+            List<int> accessEventTypes = new List<int>() { 1280, 1288, 1313 };
+            List<int> userIDs = new List<int>() { 176, 138 };
+            events = CDVIDatabase.GetUserEvents(from, to, accessEventTypes, userIDs);
+
+            Event t = events.FirstOrDefault();
+            string eventOutput = $"UserID: {t.UserNameID.ToString()}" + $" Count: {events.Count.ToString()}" + $" TimeStamp: {t.FieldTime.ToString()}";
             Console.WriteLine(eventOutput);
+            log.Info(eventOutput);
             Console.ReadLine();
 
             string json = JsonConvert.SerializeObject(events.ToArray(), Formatting.Indented);
 
-            System.IO.File.WriteAllText(path + @"\test.txt", json);
-        }
-
-
-
-        static List<Event> GetAccessEvents(DateTime from, DateTime to)
-        {
-            List<Event> events = new List<Event>();
-            //@TODO:
-            return events;
-        }
-
-
-
-        static List<Event> GetYesterdaysEvents(string connectionString)
-        {
-            string query = @"SELECT  [Event ID],[Event Type],[Field Time],[Logged Time],[Operator ID],[Card Holder ID],[Record Name ID],[Site Name ID],[UserNameID]"
-                            + @"  FROM [Centaur3Events].[dbo].[Events]"
-                            + @"  WHERE CAST([Field Time] as date) = DATEADD(day, -1, convert(date, GETDATE()))";
-            List<Event> events = new List<Event>();
-            using (var conn = new SqlConnection(connectionString))
-            {
-                conn.Open();
-                using (IDatabase db = new Database(conn))
-                {
-                    var result = db.Fetch<Event>(query);
-                    events = result;
-                }
-                conn.Close();
-            }
-            return events;
+            System.IO.File.WriteAllText(appPath + @"\App_Data\test.txt", json);
         }
 
 
