d3da827b9e4965f743b4cdca97b4a59d4a0fd0d9
ablankholm <bo.bojsen@gmail.com>
2017-07-04 16:38:49 +0100

    Refactored queries and updated tests

--- Lyca2CoreHrApiTask/DAL/CDVIRepository.cs
+++ Lyca2CoreHrApiTask/DAL/CDVIRepository.cs
@@ -13,29 +13,30 @@ using System.Linq.Expressions;
 
 namespace Lyca2CoreHrApiTask.DAL
 {
-    public class CDVIDatabase
+    public enum Cardinality
     {
-        private static Logger log = LogManager.GetCurrentClassLogger();
-        private static string connectionString = ConfigurationManager.AppSettings["CDVI:ConnectionString"];
+        Descending = -1,
+        None = 0,
+        Ascending = 1
+    }
 
 
 
-        public enum Cardinality
-        {
-            Descending = -1,
-            None = 0,
-            Ascending = 1
-        }
+    public class CDVIRepository
+    {
+        private static Logger log               = LogManager.GetCurrentClassLogger();
+        private static string connectionString  = ConfigurationManager.AppSettings["CDVI:ConnectionString"];
+        private static string eventsBaseQuery   = $"SELECT  [Event ID],[Event Type],[Field Time],[Logged Time],[Operator ID],[Card Holder ID],[Record Name ID],[Site Name ID],[UserNameID] "
+                                                + $"FROM [Centaur3Events].[dbo].[Events] ";
 
 
 
-        public static List<Event> GetEvents(List<int> eventIDs)
+        public List<Event> GetEvents(List<int> eventIDs)
         {
             List<Event> events = new List<Event>();
             string EventIDs = String.Join(",", eventIDs);
-            string query = $"SELECT  [Event ID],[Event Type],[Field Time],[Logged Time],[Operator ID],[Card Holder ID],[Record Name ID],[Site Name ID],[UserNameID]"
-                + $"  FROM [Centaur3Events].[dbo].[Events]"
-                + $"  WHERE [Event ID] IN ({EventIDs})";
+            string query = eventsBaseQuery + $"WHERE [Event ID] IN ({EventIDs})";
+            log.Info($"GetEvents(eventIDs: {EventIDs}) executing query : {query}");
             using (var conn = new SqlConnection(connectionString))
             {
                 conn.Open();
@@ -51,17 +52,12 @@ namespace Lyca2CoreHrApiTask.DAL
 
 
 
-        public static List<Event> GetEvents(int fromEventId, Cardinality toEndOfTable = Cardinality.Ascending)
+        public List<Event> GetEvents(int fromEventId, Cardinality toEndOfTable = Cardinality.Ascending)
         {
             List<Event> events = new List<Event>();
-            string opStr = @">=";
-            if (toEndOfTable == Cardinality.Descending)
-            {
-                opStr = @"<=";
-            }
-            string query = $"SELECT  [Event ID],[Event Type],[Field Time],[Logged Time],[Operator ID],[Card Holder ID],[Record Name ID],[Site Name ID],[UserNameID]"
-                            + $"  FROM [Centaur3Events].[dbo].[Events]"
-                            + $"  WHERE [Event ID] {opStr} {fromEventId}";
+            string opStr = (toEndOfTable == Cardinality.Descending) ? @">=" : "<=";
+            string query = eventsBaseQuery + $"WHERE [Event ID] {opStr} {fromEventId}";
+            log.Info($"GetEvents(fromEventId: {fromEventId}, toEndOfTable: {toEndOfTable.ToString()}) executing query : {query}");
             using (var conn = new SqlConnection(connectionString))
             {
                 conn.Open();
@@ -77,16 +73,16 @@ namespace Lyca2CoreHrApiTask.DAL
 
 
 
-        public static List<Event> GetEvents(DateTime from, DateTime to, List<int> eventTypes)
+        public List<Event> GetEvents(DateTime from, DateTime to, List<int> eventTypes)
         {
             List<Event> events = new List<Event>();
             string From = from.ToString("yyyy-MM-dd HH:mm:ss.fff");
             string To = to.ToString("yyyy-MM-dd HH:mm:ss.fff");
             string EventTypes = String.Join(",", eventTypes);
-            string query = $"SELECT  [Event ID],[Event Type],[Field Time],[Logged Time],[Operator ID],[Card Holder ID],[Record Name ID],[Site Name ID],[UserNameID]"
-                + $"  FROM [Centaur3Events].[dbo].[Events]"
-                + $"  WHERE ([Field Time] BETWEEN CONVERT(datetime, '{From}') AND CONVERT(datetime, '{To}'))"
-                + $"      AND [Event Type] IN ({EventTypes})";
+            string query = eventsBaseQuery
+                        + $"WHERE ([Field Time] BETWEEN CONVERT(datetime, '{From}') AND CONVERT(datetime, '{To}')) "
+                        + $"AND [Event Type] IN ({EventTypes})";
+            log.Info($"GetEvents(from: {From}, to: {To}, eventTypes: {EventTypes}) executing query : {query}");
             using (var conn = new SqlConnection(connectionString))
             {
                 conn.Open();
@@ -102,22 +98,18 @@ namespace Lyca2CoreHrApiTask.DAL
 
 
 
-        public static List<Event> GetEvents(DateTime from, DateTime to, List<int> eventTypes, List<uint> userIDs)
+        public List<Event> GetEvents(DateTime from, DateTime to, List<int> eventTypes, List<int> userIDs)
         {
             List<Event> events = new List<Event>();
             string From = from.ToString("yyyy-MM-dd HH:mm:ss.fff");
             string To = to.ToString("yyyy-MM-dd HH:mm:ss.fff");
             string EventTypes = String.Join(",", eventTypes);
             string UserIDs = String.Join(",", userIDs);
-            Console.WriteLine($"from : {From} / {from}, to: {To} / {to}");
-            log.Info($"from : {From} / {from}, to: {To} / {to}");
-            Console.WriteLine($"types: {EventTypes}");
-            log.Info($"types: {EventTypes}");
-            string query = $"SELECT  [Event ID],[Event Type],[Field Time],[Logged Time],[Operator ID],[Card Holder ID],[Record Name ID],[Site Name ID],[UserNameID]"
-                + $"  FROM [Centaur3Events].[dbo].[Events]"
-                + $"  WHERE ([Field Time] BETWEEN CONVERT(datetime, '{From}') AND CONVERT(datetime, '{To}'))"
-                + $"      AND [Event Type] IN ({EventTypes})"
-                + $"      AND [UserNameID] IN ({UserIDs})";
+            string query = eventsBaseQuery
+                        + $"WHERE ([Field Time] BETWEEN CONVERT(datetime, '{From}') AND CONVERT(datetime, '{To}')) "
+                        + $"AND [Event Type] IN ({EventTypes}) "
+                        + $"AND [UserNameID] IN ({UserIDs})";
+            log.Info($"GetEvents(from: {From}, to: {To}, eventTypes: {EventTypes}, userIDs: {UserIDs}) executing query : {query}");
             using (var conn = new SqlConnection(connectionString))
             {
                 conn.Open();
@@ -133,14 +125,14 @@ namespace Lyca2CoreHrApiTask.DAL
 
 
 
-        public static List<Event> GetAccessEvents(DateTime from, DateTime to)
+        public List<Event> GetAccessEvents(DateTime from, DateTime to)
         {
             return GetEvents(from, to, new List<int> { 1280, 1288, 1313 });
         }
 
 
 
-        public static List<Event> GetEventsByUsers(DateTime from, DateTime to, List<int> eventTypes, List<uint> userIDs)
+        public List<Event> GetEventsByUsers(DateTime from, DateTime to, List<int> eventTypes, List<int> userIDs)
         {
             return GetEvents(from, to, eventTypes, userIDs);
         }
--- Lyca2CoreHrApiTask/Program.cs
+++ Lyca2CoreHrApiTask/Program.cs
@@ -21,7 +21,7 @@ namespace Lyca2CoreHrApiTask
     {
         private static Logger log = LogManager.GetCurrentClassLogger();
         private static string appPath = Application.StartupPath;
-        private static CDVIDatabase db = new CDVIDatabase();
+        private static CDVIRepository CDVI = new CDVIRepository();
 
         static int Main(string[] args)
         {
@@ -32,12 +32,16 @@ namespace Lyca2CoreHrApiTask
                 FullName = "Lyca to CoreHR Clocking API Automated Middleware"
             };
 
+
+
             app.OnExecute(() => 
             {
                 app.ShowHelp();
                 Test();
                 return 2;
             });
+
+
 
             //Retrieve configuration
             //@TODO
@@ -53,10 +57,10 @@ namespace Lyca2CoreHrApiTask
 
             //Iterative testing during dev
 
+
+
             app.Command("RunDevTest", c => {
-
                 c.Description = "Runs the currently configured test method for iterative development";
-
                 c.HelpOption("-?|-h|--help");
 
                 c.OnExecute(() => {
@@ -65,34 +69,11 @@ namespace Lyca2CoreHrApiTask
 
                     return 0;
                 });
-
             });
 
+
+
             return app.Execute(args);
-        }
-
-
-
-        //@TempTesting
-        static void Test()
-        {
-            List<Event> events = new List<Event>();
-            DateTime today = DateTime.Today;
-            DateTime from = new DateTime(today.Year, today.Month, today.Day, 0, 0, 0, 0);
-            DateTime to = new DateTime(today.Year, today.Month, today.Day, 23, 59, 59, 999);
-            List<int> accessEventTypes = new List<int>() { 1280, 1288, 1313 };
-            List<int> userIDs = new List<int>() { 176, 138 };
-            events = CDVIDatabase.GetUserEvents(from, to, accessEventTypes, userIDs);
-
-            Event t = events.FirstOrDefault();
-            string eventOutput = $"UserID: {t.UserNameID.ToString()}" + $" Count: {events.Count.ToString()}" + $" TimeStamp: {t.FieldTime.ToString()}";
-            Console.WriteLine(eventOutput);
-            log.Info(eventOutput);
-            Console.ReadLine();
-
-            string json = JsonConvert.SerializeObject(events.ToArray(), Formatting.Indented);
-
-            System.IO.File.WriteAllText(appPath + @"\App_Data\test.txt", json);
         }
 
 
@@ -100,6 +81,60 @@ namespace Lyca2CoreHrApiTask
         public void PostClockingData()
         {
             //@TODO
+        }
+
+
+
+        //@TempTesting (refactor out to a dedicated testing module)
+        static void Test()
+        {
+            List<Event> el = new List<Event>();
+            Event e = new Event();
+            List<int> eventIDs = new List<int> { 9999, 12999 };
+            int eventID = 1434381;
+            DateTime today = DateTime.Today;
+            DateTime from = new DateTime(today.Year, today.Month, today.Day, 0, 0, 0, 0);
+            DateTime to = new DateTime(today.Year, today.Month, today.Day, 23, 59, 59, 999);
+            List<int> accessEventTypes = new List<int>() { 1280, 1288, 1313 };
+            List<int> userIDs = new List<int>() { 176, 138 };
+
+
+            //Test repository methods
+            el = CDVI.GetEvents(eventIDs);
+            e = el.FirstOrDefault();
+            log.Debug($"Count: {el.Count}, First Record[ EventID: {e.EventID}, EventType: {e.EventType}, FieldTime: {e.FieldTime}, UserNameID: {e.UserNameID}]");
+            el.Clear();
+
+
+            el = CDVI.GetEvents(eventID, Cardinality.Ascending);
+            e = el.FirstOrDefault();
+            log.Debug($"Count: {el.Count}, First Record[ EventID: {e.EventID}, EventType: {e.EventType}, FieldTime: {e.FieldTime}, UserNameID: {e.UserNameID}]");
+            el.Clear();
+
+
+            el = CDVI.GetEvents(from, to, accessEventTypes);
+            e = el.FirstOrDefault();
+            log.Debug($"Count: {el.Count}, First Record[ EventID: {e.EventID}, EventType: {e.EventType}, FieldTime: {e.FieldTime}, UserNameID: {e.UserNameID}]");
+            el.Clear();
+
+
+            el = CDVI.GetEvents(from, to, accessEventTypes, userIDs);
+            e = el.FirstOrDefault();
+            log.Debug($"Count: {el.Count}, First Record[ EventID: {e.EventID}, EventType: {e.EventType}, FieldTime: {e.FieldTime}, UserNameID: {e.UserNameID}]");
+            el.Clear();
+
+
+            //Test file output
+            List<Event> events = new List<Event>();
+            events = CDVI.GetEvents(from, to, accessEventTypes, userIDs);
+            e = events.FirstOrDefault();
+            string eventOutput = $"UserID: {e.UserNameID.ToString()}" + $" Count: {events.Count.ToString()}" + $" TimeStamp: {e.FieldTime.ToString()}";
+            Console.WriteLine(eventOutput);
+            log.Info(eventOutput);
+            Console.ReadLine();
+
+            string json = JsonConvert.SerializeObject(events.ToArray(), Formatting.Indented);
+            System.IO.File.WriteAllText(appPath + @"\App_Data\test.txt", json);
         }
     }
 }
